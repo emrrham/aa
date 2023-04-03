b5becf98a0dcc50ef6d3840619036545ae8cbcdd
emrrham <mremir255@gmail.com>
2023-04-03 17:00:48 +0300

    asalsaiy

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sayı girin : ");
+        int sayi = input.nextInt();
+        for (int i = 2; i<=sayi / 2;i++)
+        if (sayi % i ==0){
+
+        }
+    }
+
+}
